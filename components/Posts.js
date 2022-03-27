1bf901e904a3e49b7d01f0e7d3688c85d76c8ca5
Avery King <avery.n.king@wmich.edu>
2022-03-27 00:42:01 -0400

    ``Post`` array mapped in ``Posts``

--- components/Posts.js
+++ components/Posts.js
@@ -1,9 +1,28 @@
 import React from 'react';
+import Post from "./Post";
 
+const posts = [
+    {
+        id:'123',
+        username:'avery',
+        userImage: '',
+        image: '',
+        message: 'hello world!'
+
+    }
+]
 const Posts = () => {
     return (
         <div>
-            
+            {posts.map(post => (
+                <Post key={post.id}
+                username={post.username}
+                id={post.id}
+                userImage={post.userImage}
+                image={post.image}
+                message={post.message}
+                />
+            ))}
         </div>
     );
 };
