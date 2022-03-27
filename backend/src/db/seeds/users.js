1848507fbe04d7d4744ab17dae02bd16ecf4e40d
Avery King <avery.n.king@wmich.edu>
2022-03-27 19:24:29 -0400

    users seed switched from delete to trunctate

--- backend/src/db/seeds/users.js
+++ backend/src/db/seeds/users.js
@@ -3,7 +3,7 @@
  * @returns { Promise<void> } 
  */
 exports.seed = async function(knex) {
-  await knex('users').del()
+  await knex('users').truncate();
   await knex('users').insert([
     { username:'avery', email: 'kavery086@gmail.com',password:'DEBUG' },
   ]);
