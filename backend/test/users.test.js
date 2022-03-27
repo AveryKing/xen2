2b93318602ab98a263666d5f10c2472de0fe10d2
Avery King <avery.n.king@wmich.edu>
2022-03-27 17:32:42 -0400

    jest setup

--- backend/test/users.test.js
+++ backend/test/users.test.js
@@ -1,11 +1,46 @@
+require('dotenv').config();
 const supertest = require('supertest');
 const  usersRouter = require('../src/users/users.router');
 const makeTestApp = require("./makeTestApp");
+const app = makeTestApp('/users', usersRouter);
 
 describe("users route", () => {
-    const app = makeTestApp('/users', usersRouter);
     it("GET /:userId returns 404 if user does not exist" , async () => {
-        const userId = 1337;
-        await supertest(app).get(`/users/${userId}`).expect(404);
+        const userId = 99999;
+        const response = await supertest(app).get(`/users/${userId}`);
+        expect(response.status).toBe(404);
+        expect(response.body.error).toBeDefined();
+        expect(response.body.error).toContain('not found');
+    });
+
+    it("Request body includes data", async () => {
+        const response = await supertest(app)
+            .post('/users')
+            .set('Accept', 'application/json')
+            .send();
+        expect(response.body.error).toBeDefined();
+        expect(response.body.error).toBe('Request body is malformed');
+    })
+
+    describe("Cannot create an account without required parameters", () => {
+        const requiredParameters = ['email', 'username', 'password'];
+        requiredParameters.forEach(parameter => {
+            it(`validates presence of ${parameter} parameter`, async () => {
+                const params =  requiredParameters.filter(x => x !== parameter);
+                const response = await supertest(app)
+                    .post('/users')
+                    .set('Accept', 'application/json')
+                    .send({data:params.reduce((acc, param) => {
+                            acc[param] = 'data';
+                            return acc;
+                        }, {})});
+                expect(response.status).toBe(400);
+                expect(response.body.error).toBeDefined();
+                requiredParameters.filter(x => !params.includes(x))
+                    .forEach(param => expect(response.body.error).toContain(param));
+
+            })
+        })
+
     })
 })
