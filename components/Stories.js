c747d3f3d3f5cdb281cd71c17e47567f2323b009
Avery King <avery.n.king@wmich.edu>
2022-03-26 22:13:48 -0400

    faker added

--- components/Stories.js
+++ components/Stories.js
@@ -1,9 +1,17 @@
-import React from 'react';
+import React, {useEffect} from 'react';
+import faker from "@faker-js/faker";
 
 const Stories = () => {
+    useEffect( () => {
+        const fakes = [];
+        for (let i=0;i<20;i++) {
+            fakes.push({...faker.helpers.contextualCard(),id:i})
+        }
+        console.log(fakes)
+    }, [])
     return (
         <div>
-            
+
         </div>
     );
 };
