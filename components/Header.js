f495a08c0183f70cc3d052b207e543003d214aec
Avery King <avery.n.king@wmich.edu>
2022-03-26 21:06:19 -0400

    -

--- components/Header.js
+++ components/Header.js
@@ -8,11 +8,12 @@ import {
     PaperAirplaneIcon,
     MenuIcon
 } from "@heroicons/react/outline";
+import {HomeIcon} from "@heroicons/react/solid";
 
 const Header = () => {
     return (
         <div>
-            <div className='flex justify-between max-w-6xl'>
+            <div className='flex justify-between max-w-6xl mx-5 lg:mx-auto '>
                 <div className='relative hidden lg:inline-grid w-24 cursor-pointer'>
                     <Image
                         src='/instagram-logo.png'
@@ -29,14 +30,22 @@ const Header = () => {
                 </div>
 
                 {/* Search */}
-                <div className='mt-1 relative p-3 rounded-md'>
-                    <div className='absolute inset-y-0 pl-3 flex items-center pointer-events-none'>
-                        <SearchIcon className='h-5 w-5 text-gray-500' />
+                <div className='max-w-xs'>
+                    <div className='mt-1 relative p-3 rounded-md'>
+                        <div className='absolute inset-y-0 pl-3 flex items-center pointer-events-none'>
+                            <SearchIcon className='h-5 w-5 text-gray-500' />
+                        </div>
+                        <input className='bg-gray-50 block w-full pl-10 sm:text-sm border-gray-300 rounded-md focus:ring-black focus:border-black' type="text" placeholder='Search' />
                     </div>
-                    <input className='bg-gray-50 block w-full pl-10 sm:text-sm border-gray-300 focus:ring-black focus:border-black' type="text" placeholder='Search' />
                 </div>
 
+
                 {/* Right */}
+                <div className='flex items-center justify-end space-x-4'>
+                    <HomeIcon className='h-10 w-10'/>
+                    <MenuIcon className='h-6 md:hidden cursor-pointer'/>
+                </div>
+
             </div>
         </div>
     );
