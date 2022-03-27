0e442bdf2592d99f86975c46e7f99bb4082f7323
Avery King <avery.n.king@wmich.edu>
2022-03-27 01:45:17 -0400

    feed grid fixed

--- components/Post.js
+++ components/Post.js
@@ -11,14 +11,29 @@ import {
 import { HeartIcon as HeartIconFilled } from '@heroicons/react/solid'
 const Post = ({username, id, userImage,message,image}) => {
     return (
-        <div className='bg-white my-7 border rounded-sm'>
-            {/* Header */ }
-            <div className='flex items-center p-5'>
-                <img className='rounded-full h-12 w-12 object-contain p-1 mr-3 border' src={userImage} alt=""/>
+        <div className="bg-white my-7 border rounded-sm">
+            {/* Header */}
+            <div className="flex items-center p-5">
+                <img
+                    src={userImage}
+                    alt=""
+                    className="rounded-full h-12 w-12 object-contain border p-1 mr-3 "
+                />
+                <p className="flex-1 font-bold">{username}</p>
 
-                <p className='flex-1 font-bold'>{username}</p>
-                <DotsHorizontalIcon className='h-5'/>
+                <DotsHorizontalIcon className="h-5 cursor-pointer" />
             </div>
+
+            {/* img */}
+
+            <img src={image} alt="" className="object-cover w-full" />
+
+            {/* Buttons */}
+                <div>
+                    <HeartIcon className='btn'/>
+                    <ChatIcon className='btn'/>
+                    <PaperAirplaneIcon class='btn'/>
+                </div>
         </div>
     );
 };
--- components/Posts.js
+++ components/Posts.js
@@ -6,7 +6,7 @@ const posts = [
         id:'123',
         username:'avery',
         userImage: 'https://avatars.githubusercontent.com/u/76629826?v=4',
-        image: '',
+        image: 'https://images.immediate.co.uk/production/volatile/sites/7/2018/09/GettyImages-3227593_CROP-0005720.jpg',
         message: 'hello world!'
 
     }
