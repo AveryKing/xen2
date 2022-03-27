ff2bf9b049abdd243dee5a610eda03d0b0a4bd3c
Avery King <avery.n.king@wmich.edu>
2022-03-27 19:11:59 -0400

    testing environment configured

--- backend/test/users.test.js
+++ backend/test/users.test.js
@@ -3,9 +3,8 @@ const supertest = require('supertest');
 const  usersRouter = require('../src/users/users.router');
 const makeTestApp = require("./makeTestApp");
 const app = makeTestApp('/users', usersRouter);
-
-describe("users route", () => {
-    it("GET /:userId returns 404 if user does not exist" , async () => {
+const knex = require('../src/db/connection')
+    test("GET /:userId returns 404 if user does not exist" , async () => {
         const userId = 99999;
         const response = await supertest(app).get(`/users/${userId}`);
         expect(response.status).toBe(404);
@@ -13,19 +12,22 @@ describe("users route", () => {
         expect(response.body.error).toContain('not found');
     });
 
-    it("Request body includes data", async () => {
-        const response = await supertest(app)
-            .post('/users')
-            .set('Accept', 'application/json')
-            .send();
-        expect(response.body.error).toBeDefined();
-        expect(response.body.error).toBe('Request body is malformed');
-    })
 
-    describe("Cannot create an account without required parameters", () => {
+
+    describe("Account creation", () => {
+        test("data required in request.body", async () => {
+            const response = await supertest(app)
+                .post('/users')
+                .set('Accept', 'application/json')
+                .send();
+            expect(response.body.error).toBeDefined();
+            expect(response.body.error).toBe('Request body is malformed');
+        })
+
+        knex.seed.run();
         const requiredParameters = ['email', 'username', 'password'];
         requiredParameters.forEach(parameter => {
-            it(`validates presence of ${parameter} parameter`, async () => {
+            test(`${parameter} parameter required`, async () => {
                 const params =  requiredParameters.filter(x => x !== parameter);
                 const response = await supertest(app)
                     .post('/users')
@@ -41,6 +43,20 @@ describe("users route", () => {
 
             })
         })
-
     })
-})
+
+    test("POST to /users creates a new account", async () => {
+        const response = await supertest(app)
+            .post('/users/')
+            .set('Accept','application/json')
+            .send({
+                data: {
+                    username:'username123',
+                    email:'email123456',
+                    password:'password321'
+                }
+            });
+        console.log(response.body)
+        expect(response.body.error).toBeUndefined();
+        expect(response.body.data).toContain('successfully');
+    })
