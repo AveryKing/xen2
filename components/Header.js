1f7a04a46b42f008ac93a42e6325f7280a3e94b6
Avery King <avery.n.king@wmich.edu>
2022-03-26 21:27:50 -0400

    nav icons complete

--- components/Header.js
+++ components/Header.js
@@ -45,6 +45,9 @@ const Header = () => {
                     <HomeIcon className='navBtn'/>
                     <MenuIcon className='h-6 md:hidden cursor-pointer'/>
                     <PaperAirplaneIcon className='navBtn'/>
+                    <PlusCircleIcon className='navBtn'/>
+                    <UserGroupIcon className='navBtn'/>
+                    <HeartIcon className='navBtn'/>
                 </div>
 
             </div>
