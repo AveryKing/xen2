06b53e2ab8ffbaae9f1d3da043711eae724be0ba
Avery King <avery.n.king@wmich.edu>
2022-03-27 22:28:49 -0400

    username validation tests implemented

--- backend/test/users.test.js
+++ backend/test/users.test.js
@@ -11,27 +11,6 @@ const testUserData = require('../src/db/test/config/testUserData')
  * Include --runInBand flag when running Jest for tests to pass!
  */
 
-describe('list', () => {
-    test('GET / returns a list of all users', async () => {
-        await knex.seed.run()
-        const {body: {data}} = await supertest(app).get('/users');
-        for (let i in testUserData) {
-            expect(data[i].username).toEqual(testUserData[i].username);
-        }
-
-    })
-})
-
-describe('read', () => {
-    test("GET /:userId returns 404 if user does not exist", async () => {
-        const userId = 99999;
-        const response = await supertest(app).get(`/users/${userId}`);
-        expect(response.status).toBe(404);
-        expect(response.body.error).toBeDefined();
-        expect(response.body.error).toContain('not found');
-    });
-})
-
 describe("create", () => {
     describe('request validation', () => {
         test("data required in request.body", async () => {
@@ -63,6 +42,32 @@ describe("create", () => {
 
             })
         })
+            describe('username validation', () => {
+                // Username datasets:
+                const usernameDatasets = [
+                    {test: 'username cannot be empty', value: ''},
+                    {test: 'username cannot be less than 3 characters', value: '12'},
+                    {test: 'username cannot be greater than 10 characters', value: 'qwertyuiopasdfgh'},
+                    {test: 'username must be unique', value: testUserData[0].username},
+                ]
+                for(let i in usernameDatasets) {
+                    test(usernameDatasets[i].test, async () => {
+                        const response = await supertest(app)
+                            .post('/users')
+                            .set('Accept', 'application/json')
+                            .send({
+                                data: {
+                                    username: usernameDatasets[i].value,
+                                    email:'testemail@gmail.com',
+                                    password:'iamapassword'
+                                },
+                            });
+                        expect(response.status).toBe(400);
+                        expect(response.body.error).toBeDefined();
+                        expect(response.body.error).toContain('username');
+                    })
+                }
+            })
     })
 
     describe("POST to /users creates a new account", () => {
@@ -96,3 +101,24 @@ describe("create", () => {
 
 })
 
+describe('read', () => {
+    test("GET /:userId returns 404 if user does not exist", async () => {
+        const userId = 99999;
+        const response = await supertest(app).get(`/users/${userId}`);
+        expect(response.status).toBe(404);
+        expect(response.body.error).toBeDefined();
+        expect(response.body.error).toContain('not found');
+    });
+})
+
+
+describe('list', () => {
+    test('GET / returns a list of all users', async () => {
+        await knex.seed.run()
+        const {body: {data}} = await supertest(app).get('/users');
+        for (let i in testUserData) {
+            expect(data[i].username).toEqual(testUserData[i].username);
+        }
+
+    })
+})
