16c44d406dddd27ac09a3cff05616228bc945596
Avery King <kavery086@gmail.com>
2022-03-31 04:11:23 -0400

    password validation implemented with bcrypt.compare

--- backend/src/users/users.service.js
+++ backend/src/users/users.service.js
@@ -7,28 +7,30 @@ const list = () => {
 }
 
 // Inserts new user into database
-const create = async (user) => {
-    await hashPassword(user.password)
+const create =  (user) => {
+     hashPassword(user.password)
         .then(hash => {
             user.password = hash;
         })
-    return knex('users').insert(user,'id');
+    return knex('users').insert(user, 'id');
 }
 
-const hashPassword = async (password) => {
-    const salt = "testlol";
-    return await bcrypt.hash(password, 10)
+const validatePassword =  (plaintext, hash) => {
+    return bcrypt.compare(plaintext, hash);
+}
+const hashPassword =  (password) => {
+    return bcrypt.hash(password, 10)
 }
 // Retrieves a user's data from database
 const read = (id) => {
     return knex('users')
         .select('*')
-        .where('id','=',id);
+        .where('id', '=', id);
 }
 
 const isUsernameTaken = (username) => {
     return knex('users').select('id')
-        .where('username','=', username).length === 0
+        .where('username', '=', username).length === 0
 }
 module.exports = {
     list,
