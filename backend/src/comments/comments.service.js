e7b1e9d9d01793f439c47a8dfcf6e6bdcbf87741
Avery King <kavery086@gmail.com>
2022-04-18 20:07:34 -0400

    comments service added

---
+++ backend/src/comments/comments.service.js
@@ -0,0 +1,35 @@
+const knex = require('../db/connection');
+
+function create(comment) {
+  return knex('comments')
+    .insert(comment, '*')
+    .then(comment => comment[0]);
+}
+
+function list(postId) {
+    return knex('comments')
+        .select('*')
+        .where('post', postId)
+        .orderBy('created_at', 'desc')
+        .then(comments => comments)
+}
+
+function read(commentId) {
+    return knex('comments')
+        .select('*')
+        .where('id', commentId)
+        .then(comment => comment[0])
+}
+
+function remove(commentId) {
+    return knex('comments')
+        .where('id', commentId)
+        .delete();
+}
+
+module.exports = {
+    create,
+    list,
+    read,
+    remove
+}
