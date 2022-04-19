88a7ad8a2d4449ea78c0c1d3e652b4c92a6b7b2f
Avery King <kavery086@gmail.com>
2022-04-19 05:16:22 -0400

    comment read/list tests passing

--- backend/src/db/config/testCommentData.js
+++ backend/src/db/config/testCommentData.js
@@ -1,10 +1,9 @@
 
-const service = require('../../posts/posts.service');
-const posts = [
+const comments = [
     {post: 1, creator: 1, content: 'I am a comment', likes:[]},
     {post: 2, creator: 1, content: 'I am also a comment', likes:[]},
     {post: 2, creator: 1, content: 'I am also a comment222', likes:[]},
     {post: 3, creator: 2, content: 'I am a comment too', likes:[5]}
 ]
 
-module.exports = posts;
+module.exports = comments;
--- backend/test/comments.test.js
+++ backend/test/comments.test.js
@@ -1,25 +1,38 @@
 require('dotenv').config();
 const supertest = require('supertest');
-const postsRouter = require('../src/posts/posts.router');
+const commentsRouter = require('../src/comments/comments.router');
 const makeTestApp = require("./makeTestApp");
-const service = require('../src/posts/posts.service');
-const app = makeTestApp('/posts', postsRouter);
+const service = require('../src/comments/comments.service');
+const app = makeTestApp('/comments', commentsRouter);
 const knex = require('../src/db/connection');
-const testPostData = require("../src/db/config/testPostData");
+const testCommentData = require("../src/db/config/testCommentData");
 const {load} = require("nodemon/lib/rules");
 jest.setTimeout(100000);
 
 describe('list', () => {
-    test('GET /:postId returns all comments', async () => {
-
-    })
-    test('Comments are relevant to queried post', async () => {
-
-    })
-})
+    test('GET /:postId returns relevant comments', async () => {
+        await knex.seed.run();
+        await supertest(app)
+        .get('/comments/2')
+        .then(res => {
+            expect(res.body.comments.length).toBe(testCommentData.filter(x => x.post === 2).length);
+            for(let comment of res.body.comments) {
+                expect(testCommentData.some(x => x.content === comment.content)).toBe(true);
+            }
+        })
+    })})
 
 describe('read', () => {
     test('GET /:postId/:commentId returns a comment', async () => {
-
+        await supertest(app)
+        .get(`/comments/1/1`)
+        .then(res => {
+            console.log(res.body);
+            expect(res.body.error).toBeUndefined();
+            expect(res.body.comment).toBeDefined();
+            expect(res.body.comment.content).toBe(testCommentData[0].content);
+            expect(res.body.comment.creator).toBe(testCommentData[0].creator);
+            expect(res.body.comment.post).toBe(testCommentData[0].post);
+    })
     })
 })
